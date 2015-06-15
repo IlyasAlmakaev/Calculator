70555d991b6acc9a899986de3d854ad614ab662b
IlyasAlmakaev <i.almakaev@gmail.com>
2015-06-15 12:32:58 +0700

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by almakaev iliyas on 15.06.15.
+//  Copyright (c) 2015 almakaev iliyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by almakaev iliyas on 15.06.15.
+//  Copyright (c) 2015 almakaev iliyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
